36a497ce034dd82c487d6875763b65dab346beb8
fordaj <26288298+fordaj@users.noreply.github.com>
2021-09-18 12:08:44 -0600

    Added back button

--- indexer.py
+++ indexer.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PY_FILE_PATH = afpy.Cli().cwd(__file__)
 FILES = 'Files'
 
-files_to_ignore = ['.DS_Store', '.git', '.vscode', 'indexer.py', 'index.md']
+files_to_ignore = ['.DS_Store', '.git', '.vscode', 'indexer.py', 'index.md', 'script.js']
 
 def index(dir_path, file_structure):
     file_structure[FILES] = []
@@ -21,7 +21,8 @@ def index(dir_path, file_structure):
     if os.path.exists(str(index_path)):
         os.remove(str(index_path))
     index_md = open(str(index_path),'w')
-    index_md.write(f"[Back](../index.md)\n\n")
+    if str(dir_path).split('/')[-1] != "fordnotes":
+        index_md.write(f"[Back](../index.md)\n\n")
     index_md.write(f"# {str(dir_path).split('/')[-1]}\n\n")
     dirs = list(file_structure.keys())
     dirs.sort()
@@ -34,6 +35,21 @@ def index(dir_path, file_structure):
     for file in files:
         formatted_file = file.split('.')[0].replace('_',' ')
         index_md.write(f"- [{formatted_file}]({file})\n")
+        file_md = open(str(dir_path/file))
+        try:
+            lines = file_md.readlines()
+            if '[Back](index.md)' not in lines[0]:
+                lines.insert(0,f"[Back](index.md)\n\n")
+                file_md.seek(0)
+                file_md.close()
+                file_md = open(str(dir_path/file),'w')
+                file_md.writelines(lines)
+        except:
+            # File is not readable (likely empty)
+            file_md.close()
+            file_md = open(str(dir_path/file),'w')
+            file_md.seek(0)
+            file_md.write("[Back](index.md)\n\n")
 
 file_structure = {}
 
