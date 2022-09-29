c3516bf5f869f7930a3762112512e177be38eead
fordaj <andy@ford-network.com>
2022-09-29 13:49:46 -0400

    Added NavBar, Mermaid, Content sections. Revert here for a clean build.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>NavBar</p>
+    );
+  }
+}
+
+class Mermaid extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>Mermaid</p>
+    );
+  }
+}
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>Content</p>
+    );
+  }
+}
+
+const AppJSX = (
+  <div className="App">
+    <NavBar />
+    <Mermaid />
+    <Content />
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return AppJSX;
 }
 
 export default App;
