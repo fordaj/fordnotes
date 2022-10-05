caf627bb1b00c87de75b2f9cd5d145bc3a0a3b21
fordaj <andy@ford-network.com>
2022-10-05 15:46:47 -0400

    Cleaned up unnecessary code

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,16 @@
 import React from "react";
+import './NavBar.css';
 
 export default function NavBar({ Pages }) {
   return (
     <ul>
       {
-        Pages.map(
-          (Page) => {
-            if (Page.Pages !== undefined){
-              return (
-                <li key={Page.Title}>
-                  <a href={Page.URL}>{Page.Title}</a>
-                  <NavBar Pages={Page.Pages} />
-                </li>
-              );
-            }
-            return (
-              <li key={Page.Title}>
-                <a href={Page.URL}>{Page.Title}</a>
-              </li>
-            );
-          }
-        )
+        Pages.map((Page) => {return (
+          <li key={Page.Title}>
+            <a href={Page.URL}>{Page.Title}</a>
+            {Page.Pages?(<NavBar Pages={Page.Pages} />):(<></>)}
+          </li>
+        );})
       }
     </ul>
   );
