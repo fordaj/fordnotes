9411097bddaba9f04f1e838bee1360a2aadd72ce
fordaj <andy@ford-network.com>
2022-10-15 13:03:51 -0400

    Added example JSX for #4

---
+++ FileStructure.jsx
@@ -0,0 +1,15 @@
+const FileStructureJSX = {
+    Folders : {
+        "Heath": {
+            Folders : {
+                "Vitals" : {
+                    Files : ["Resting Heart Rate.md", "VO2 Max.md"],
+                },
+            },
+            Files : ["Vitamins and Supplements.md"],
+        },
+        "Computers" : {},
+        "Construction" : {},
+    },
+    Files : ["Home.md"],
+};
